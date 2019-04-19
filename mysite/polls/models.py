23341d23a16c0bb9b812e560cd67dadecd21527f
JangGeunSong <sjg1468@gmail.com>
2019-04-19 21:58:11 +0900

    add the DB

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from djongo import models
 
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Entry(models.Model):
+    post = models.EmbeddedModelField(
+        model_container = Post,
+    )
+
+    headline = models.CharField(max_length=255)
+
+    objects = models.DjongoManager()
+
+    
 # Create your models here.
